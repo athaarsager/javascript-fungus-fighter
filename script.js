5759ac2b64440a4b4d263d3a5628306b75e42b66
Andrew Haarsager <athaarsager@gmail.com>
2024-01-08 19:21:41 -0600

    Attack function successfully linked to first button

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let fungusHP = 100;
 
 function onReady() {
     console.log("Ready to go!")
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
+    const arcaneScepter = document.getElementsByClassName("arcane-scepter");
+    arcaneScepter[0].addEventListener("click", attack);
     
     
     // 🧠 Remember
@@ -20,6 +22,8 @@ function onReady() {
 onReady()
 
 function attack(e) {
+    const attackType = e.target.class;
+    alert(attackType);
     // make Freaky Fungus lose HP, reduce your AP based on given stats in README.md
     //  --check button attack class to determine the above information
     // HP and AP cannot go negative, set to zero
