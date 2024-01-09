594a1ef5a1917e8cae21d74bfa76af0d92dce564
Andrew Haarsager <athaarsager@gmail.com>
2024-01-08 19:36:17 -0600

    attack alters state correctly

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 // Feel free to make this to what you want!
 // Example:
 let fungusHP = 100;
+let heroAP = 100;
 
 function onReady() {
     console.log("Ready to go!")
-    
+
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     const attackButtons = document.getElementsByClassName("attack-btn");
     for (let button of attackButtons) {
         button.addEventListener("click", attack);
     }
-    
-    
+
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
@@ -27,6 +28,26 @@ function attack(e) {
     const attackType = e.target.className.split(" ")[1];
     // make Freaky Fungus lose HP, reduce your AP based on given stats in README.md
     //  --check button attack class to determine the above information
-    // HP and AP cannot go negative, set to zero
 
+    // display the changes in HP and AP on the DOM
+    switch (attackType) {
+        case "arcane-scepter":
+            heroAP -= 12;
+            fungusHP -= 14;
+            break;
+        case "entangle":
+            heroAP -= 23;
+            fungusHP -= 9;
+            break;
+        case "dragon-blade":
+            heroAP -= 38;
+            fungusHP -= 47;
+            break;
+        case "star-fire":
+            heroAP -= 33;
+            fungusHP -= 25;
+            break;
+    }
+    console.log(`Hero AP: ${heroAP}, fungus HP: ${fungusHP}`);
+    // HP and AP cannot go negative, set to zero
 }
